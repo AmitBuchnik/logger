971b409445d70922f199e32fe9ed417116b51ebc
Amit Buchnik <amit.buchnik@motorolasolutions.com>
2021-12-07 15:40:05 +0200

    test flush time 5 sced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ export function createLoggerConfig() {
   config.messageFormat = {
     timestampFormat: 'HH:mm:ss'
   };
-  config.flushTiming = 10;
+  config.flushTiming = 5000;
   config.useQueue = true;
   return config;
 }
